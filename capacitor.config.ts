f7134d9d31a884737fa87d49a817583c7b33c60f
GAIDaziz <gaidaz@hotmail.com>
2024-10-14 10:13:51 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.projectf',
+  appName: 'projectf',
+  webDir: 'www'
+};
+
+export default config;
